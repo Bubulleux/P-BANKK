70571d2a53d174b031a65aedeb2807cc4971640a
Zecyl <ulysses.rouet@yahoo.com>
2022-12-14 19:04:03 +0100

    Add menu_recherche

--- route/custom_widget.py
+++ route/custom_widget.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 
+
 def create_button(win, row, col, text, function):
     button = tk.Button(win, text=text, command=function)
     button.grid(row=row, column=col)
+
+
+def create_table(parent, row, col, headers, rows):
+    table = tk.Frame(parent)
+    for i, header in enumerate(headers):
+        label = tk.Label(table, text=header)
+        label.grid(0, i, padx=1, pady=1)
+    for j, row in enumerate(rows):
+        for i, value in enumerate(row):
+            label = tk.Label(table, text=value)
+            label.grid(j + 1, i, padx=1, pady=1)
+    table.grid(row, col)
 
 
 def destroy_widgets(parent):
--- route/menu_recherche.py
+++ route/menu_recherche.py
@@ -6,7 +6,17 @@ import route.custom_widget as custom_widget
 class MenuRecherche(Page):
     def __init__(self, app):
         super().__init__(app)
-        self.boutons = [(0, 1,  "Rechercher", lambda: 0)]
+        self.search_text = tk.StringVar()
+        self.comptes_courants = []
+        self.comptes_epargnes = []
+
+    def search(self):
+        return self.search_text
 
     def draw(self, window: tk.Tk):
-        pass
+        barre = tk.Entry(window, width=50, textvariable=self.search_text)
+        barre.grid(row=0, column=0)
+        custom_widget.create_button(window, 0, 1, "Rechercher", self.search)
+
+        custom_widget.create_table(window, 1, 0, self.comptes_courants[0], self.comptes_courants[1:])
+        custom_widget.create_table(window, 2, 0, self.comptes_epargnes[0], self.comptes_epargnes[1:])
