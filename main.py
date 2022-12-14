dd9e8fe6090c51141f49e3d119785679280733b9
dottedboxguy <loplop7levrai@gmail.com>
2022-12-14 15:42:16 +0100

    testing stuff

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ def test():
     print("bouton cliqué")
     
 def search(win,db):
-    widgets.menu(win,["rechercher par nom et prénom","recherche par numéro de client"],[
-        lambda : db.get_clients(input("nom prénom de la personne que vous cherchez")),
-        lambda : db.get_client_by_id(input("id du client que vous cherchez"))
-        ])
-
+    widgets.menu(win,["rechercher par nom et prénom","recherche par numéro de client"],[lambda : db.get_clients(input("nom prénom de la personne que vous cherchez")),lambda : db.get_client_by_id(input("id du client que vous cherchez"))],"Recherche")
 def look_at(win, db):
     pass
 
@@ -22,5 +18,5 @@ window = tk.Tk()
 window.title("Ma Banque")
 window.geometry("500x100")
 
-widgets.menu(window, ["Recherche","Observer un compte","Modifier des informations d'un compte"],[lambda : search(window,db),test,test],[(0,0),(1,2),(1,3)],"main menu")
+widgets.menu(window, ["Recherche","Observer un compte","Modifier des informations d'un compte"],[lambda : search(window,db),test,test],"main menu")
 window.mainloop()
