dc795bfe5c81f6553883275a8d9ad8e44e5e66e4
Zecyl <ulysses.rouet@yahoo.com>
2022-12-14 15:14:04 +0100

    wiget version ulysses

--- widgets.py
+++ widgets.py
@@ -2,19 +2,27 @@ import tkinter as tk
 
 
 def create_button(win, row, col, text, function):
-    button = tk.Button(win, text = text, command = function)
-    button.grid(row = row, column = col)
+    button = tk.Button(win, text=text, command=function)
+    button.grid(row=row, column=col)
 
 def menu_client(win, comptes_courant, comptes_epargne):
     tableau_courant = tk.Frame(win)
-    headers = [tk.Label(tableau_courant, text = key, highlightthickness = 2) for key in comptes_courant[0].keys()]
-    for i in range(len(headers)):
-        headers[i].config(highlightbackground = "red", highlightcolor= "red")
-        headers[i].grid(row = 0, column = i)
-    tableau_courant.pack()
+    tableau_epargne = tk.Frame(win)
 
+    headers = [tk.Label(tableau_courant, text=key, border="black") for key in comptes_courant[0]]
+    for i, header in enumerate(headers):
+        header.grid(row=0, column=i)
+    for i, compte in enumerate(comptes_courant[1:]):
+        labels = [tk.Label(tableau_courant, text=value, border="black") for value in compte]
+        for j, label in enumerate(labels):
+            label.grid(row=i + 1, column=j)
+    tableau_courant.grid(row=0, column=0)
 
-def menu(win, text_list, function_list, data_list,name):
-    name=tk.Label(win, text=name)
-    for i in range(len(text_list)):
-        create_button(win,data_list[i][1],data_list[i][0], text=text_list[i], function=function_list[i])
+    headers = [tk.Label(tableau_epargne, text=key, border="black") for key in comptes_epargne[0]]
+    for i, header in enumerate(headers):
+        header.grid(row=0, column=i)
+    for i, compte in enumerate(comptes_epargne[1:]):
+        labels = [tk.Label(tableau_epargne, text=value, border="black") for value in compte]
+        for j, label in enumerate(labels):
+            label.grid(row=i + 1, column=j)
+    tableau_epargne.grid(row=1, column=0)
